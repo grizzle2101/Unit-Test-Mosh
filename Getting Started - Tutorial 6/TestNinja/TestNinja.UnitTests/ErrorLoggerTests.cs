efb29ffc15ef99b598d8ade3f1fd1bc668bf40c5
Conor McGrath <conormcgrath123@gmail.com>
2018-02-12 18:44:09 +0000

    Section 3 - Tutorial 5 - Testing Void Methods

---
+++ Getting Started - Tutorial 6/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_ShouldSetLastErrorProperty()
+        {
+            //Arrange
+            var logger = new ErrorLogger();
+
+            //Act
+            logger.Log("abc");
+
+            //Assert
+            Assert.That(logger.LastError, Is.EqualTo("abc"));
+
+        }
+    }
+}
--- Getting Started - Tutorial 6/TestNinja/TestNinja/Fundamentals/ErrorLogger.cs
+++ Getting Started - Tutorial 6/TestNinja/TestNinja/Fundamentals/ErrorLogger.cs
@@ -3,17 +3,21 @@ using System;
 
 namespace TestNinja.Fundamentals
 {
+    //Section 3 - Tutorial 5 - Testing Void Methods:
     public class ErrorLogger
     {
         public string LastError { get; set; }
 
         public event EventHandler<Guid> ErrorLogged; 
         
+
         public void Log(string error)
         {
             if (String.IsNullOrWhiteSpace(error))
                 throw new ArgumentNullException();
-                
+
+            //Changing State of LastError
+            //To Test this, we test the State of LastError
             LastError = error; 
             
             // Write the log to a storage
