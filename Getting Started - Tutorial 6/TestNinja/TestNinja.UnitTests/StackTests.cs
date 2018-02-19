939df3dac9ebdd643af1c4ade47c0fa5f0a5185c
conor.mcgrath <conormcgrath123@gmail.com>
2018-02-19 17:45:51 +0000

    Section 4 - Tutorial 5 - Stack - Need to complete

---
+++ Getting Started - Tutorial 6/TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    //Section 4 - Tutorial 4 - Stack Excercise:
+    //Task 1 - Create Test Setup
+    //Task 2 - Analyze Methods for Test Scenarios
+        //-Void Method Push - Valid Object - Invalid Object
+        //-Return Method Pop - PopWhenNoItemInList & PopWhenItemInList
+    //Task 3 - Implment Test Scernarios
+    //Task 4 - Refactor if Neccessary
+
+    //Task 1 - Create Test Setup
+    [TestFixture]
+    public class StackTests
+    {
+        public Stack<string> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Arrange
+            _stack = new Stack<string>();
+        }
+
+        //Push Invalid Object
+        //Testing Exception
+        [Test]
+        public void Push_PassInvalidObject_ShouldRaiseException()
+        {
+            //Act
+            //Assert
+            var stack = new Stack<string>();
+
+            stack.Push(null);
+
+            //Issue here Null not triggering Exception, Move on & come back.
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        //Push Valid Object
+        //Testing Void Method
+        [Test]
+        public void Push_PassValidObject_ShouldAddObjectToStack()
+        {
+            //Act
+            _stack.Push("a");
+
+            //Assert
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        //Additional Test for Count, as Count could return 1 and just pass, we need to test more than 1.
+
+        //Pop Method Tests
+
+        //Peek Method Tests
+    }
+}
--- Getting Started - Tutorial 6/TestNinja/TestNinja/Fundamentals/Stack.cs
+++ Getting Started - Tutorial 6/TestNinja/TestNinja/Fundamentals/Stack.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 
 namespace TestNinja.Fundamentals
 {
+    //Section 4 - Tutorial 4 - Stack Excercise:
+    //So we have a Class here that is a Generic Stack.
     public class Stack<T>
     {
         private readonly List<T> _list = new List<T>();
 
+        //Return Count of the Stack
         public int Count => _list.Count;
 
+        //Add Object to Stack
         public void Push(T obj)
         {
             if (obj == null)
@@ -17,6 +21,7 @@ namespace TestNinja.Fundamentals
             _list.Add(obj);
         }
 
+        //Remove Object from Stack
         public T Pop()
         {
             if (_list.Count == 0)
@@ -28,7 +33,7 @@ namespace TestNinja.Fundamentals
             return result; 
         }
 
-
+        //Show Item from Stack, but not Remove.
         public T Peek()
         {
             if (_list.Count == 0)
