79cd03ecbc54cf054543d4465bf7620b7d5f8964
Conor McGrath <conormcgrath123@gmail.com>
2018-02-28 20:15:22 +0000

    Section 5 - Tutorial 3 - Refactoring towards a loosely coupled application

---
+++ Getting Started - Tutorial 6/TestNinja/TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestNinja.Mocking
+{
+    
+    public class FileReader : IFileReader
+    {
+        //Task 1 - Isolate the File Reading.
+        public string Read(string path)
+        {
+            //var str = File.ReadAllText("video.txt");
+            return File.ReadAllText(path);
+        }
+    }
+
+    //Task 2 - Extract Interface
+    public interface IFileReader
+    {
+        string Read(string path);
+    }
+}
